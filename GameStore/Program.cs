1d2d8fa49a6a773341bd941e4aedd46a1c9dd3f6
Gonzalo anuar Pérez Aramburo <gonzalo.anuar13@outlook.com>
2026-06-22 18:00:52 -0600

    feat: implement delete endpoint

--- GameStore/Program.cs
+++ GameStore/Program.cs
@@ -31,5 +31,26 @@ app.MapPost("/games",(CreateGameDto newGame) =>
    return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id},game);
 });
 
+// PUT /games/{id}
+app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) =>
+{
+    var index = games.FindIndex(game => game.Id == id); 
+    games[index] = new(id,
+    updatedGame.Name, 
+    updatedGame.Genre, 
+    updatedGame.Price, 
+    updatedGame.ReleaseDate); 
+
+    return Results.NoContent();  
+});
+
+// DELETE /games/{id}
+app.MapDelete("/games/{id}", (int id) =>
+{
+   games.RemoveAll(game => game.Id == id); 
+
+   return Results.NoContent();
+});
+
 
 app.Run();
