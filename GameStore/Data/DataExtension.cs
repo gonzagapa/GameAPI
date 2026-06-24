4fac1b86451670a6fe26f218b1dbeb3cfeb7b6ec
Gonzalo anuar Pérez Aramburo <gonzalo.anuar13@outlook.com>
2026-06-24 11:32:21 -0600

    feat: seed Database with Genre data

--- GameStore/Data/DataExtension.cs
+++ GameStore/Data/DataExtension.cs
@@ -8,7 +8,9 @@ public static class DataExtension
     //Execute pending migrations
     public static void MigrateDB(this WebApplication app)
     {
+        //Create an temporal service scope to retrieve your db context
         using var scope = app.Services.CreateScope(); 
+
         var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>(); 
         dbContext.Database.Migrate();
     }
--- GameStore/Program.cs
+++ GameStore/Program.cs
@@ -1,6 +1,7 @@
 using GameStore;
 using GameStore.Data;
 using GameStore.Endpoints;
+using GameStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddValidation();
 
 var connString = "Data Source=GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(connString);
+builder.Services.AddSqlite<GameStoreContext>(connString, optionsAction:options => options.UseSeeding((context, _) =>
+{
+    if (!context.Set<Genre>().Any())
+    {
+        context.Set<Genre>().AddRange(
+            new Genre{Name = "Fighting"}, 
+            new Genre{Name = "RPG"},
+            new Genre{Name = "Platformer"},
+            new Genre{Name = "Racing"},
+            new Genre{Name = "Sports"}
+        ); 
+
+        context.SaveChanges();
+    }
+}));
 
 var app = builder.Build();
 app.MapGamesEndpoints();
