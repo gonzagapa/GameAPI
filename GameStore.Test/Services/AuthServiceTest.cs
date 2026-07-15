ad920565f8925bafacceaaba2a37c694523b982e
Gonzalo Perez <gonzalo.anuar13@outlook.com>
2026-07-14 22:45:25 -0600

    test: finish test use cases for AuthService

--- GameStore.Test/Services/AuthServiceTest.cs
+++ GameStore.Test/Services/AuthServiceTest.cs
@@ -1,4 +1,5 @@
 using GameStore.Data;
+using GameStore.Data.Migrations;
 using GameStore.Dtos;
 using GameStore.Services;
 using Microsoft.EntityFrameworkCore;
@@ -20,7 +21,7 @@ namespace GameStore.Test.Services
         {
             var settings = new Dictionary<string, string?>
             {
-                ["AppSettings:Token"] = "test-signing-key-that-is-long-enough-1234567890",
+                ["AppSettings:Token"] = "test-signing-key-that-is-long-enough-1234567890eVeryVeryLong!!!111oneeleven",
                 ["AppSettings:Issuer"]= "TestIssuer",
                 ["AppSettings:Audience"] = "TestAudience"
             }; 
@@ -53,7 +54,83 @@ namespace GameStore.Test.Services
             var dto = new UserDto("gonzalo", "MyPassword123!");
             await service.RegisterAsync(dto); 
 
-            var secondAttempt = service.RegisterAsync8DT
+            var user = await service.RegisterAsync(dto);
+            Assert.Null(user);
+        }
+
+        [Fact]
+        public async Task LoginAsync_should_return_token_response_with_valid_credentials(){
+            using var context = CreateContext(); 
+            var service = new AuthService(CreateConfiguration(), context);
+            var userDto = new UserDto("gonzalo", "MyPassword123!");
+            await service.RegisterAsync(userDto);
+
+            var response = await service.LoginAsync(userDto);  
+            Assert.NotNull(response);
+            Assert.NotEmpty(response.AccessToken);
+            Assert.NotEmpty(response.RefreshToken);
+            Assert.False(string.IsNullOrWhiteSpace(response.AccessToken));
+            Assert.False(string.IsNullOrWhiteSpace(response.RefreshToken));
+        } 
+
+        [Fact]
+
+        public async Task LoginAsync_should_return_null_with_invalid_password()
+        {
+             using var context = CreateContext(); 
+            var service = new AuthService(CreateConfiguration(), context);
+            await service.RegisterAsync(new UserDto("gonzalo", "MyPassword123!"));
+
+            var response = await service.LoginAsync(new UserDto("gonzalo", "MyPassword124!")); 
+            Assert.Null(response);
+        }
+
+        [Fact]
+        public async Task LoginAsync_should_return_null_when_user_doesnt_exist()
+        {
+            using var context = CreateContext(); 
+            var service = new AuthService(CreateConfiguration(), context);
+            await service.RegisterAsync(new UserDto("gonzalo", "MyPassword123!")); 
+
+            var response = await service.LoginAsync(new UserDto("juanes", "MyPassword123!")); 
+            Assert.Null(response);
+        }
+        
+        [Fact]
+        public async Task Logout_should_return_string_response_with_valid_userId()
+        {
+            using var context = CreateContext(); 
+            var service = new AuthService(CreateConfiguration(), context);
+            await service.RegisterAsync(new UserDto("gonzalo", "MyPassword123!"));
+
+            var response = await service.LogoutAsync(1);
+            Assert.False(String.IsNullOrEmpty(response)); 
+        }
+
+        [Fact]
+        public async Task Logout_should_return_null_with_invalid_userId()
+        {
+             using var context = CreateContext(); 
+            var service = new AuthService(CreateConfiguration(), context);
+            await service.RegisterAsync(new UserDto("gonzalo", "MyPassword123!"));
+
+            var response = await service.LogoutAsync(3);
+            Assert.Null(response); 
+        }
+
+        [Fact]
+        public async Task Logout_should_remove_refreshToken_when_executed()
+        {
+            using var context = CreateContext(); 
+            var service = new AuthService(CreateConfiguration(), context);
+            var dto = new UserDto("gonzalo", "MyPassword123!");
+            var user = await service.RegisterAsync(dto);
+
+            await service.LoginAsync(dto);
+            await service.LogoutAsync(1); 
+
+            Assert.Null(user!.RefreshToken);
+            Assert.Null(user!.RefreshTokenExpiryTime);
         }
 
 
